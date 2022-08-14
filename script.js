bfbc1920018b8a729c581ce8b329e347aa1fb611
André_Henchenski <andrehenc@gmail.com>
2022-08-14 12:59:22 -0300

    Projeo pronto para entrega com todas as funcionalidades básicas. Daqui pra frente dá pra fazer o bonus.

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
-let prato = undefined;
-let bebida = undefined;
-let sobremesa = undefined;
+let prato = undefined
+let bebida = undefined
+let sobremesa = undefined
 
 function select(choosen) {
     if (choosen.classList.contains("prato")) {
         if (prato === choosen) {
-            choosen.classList.toggle("selected");
+            choosen.classList.toggle("selected")
         } else if (prato === undefined) {
-            choosen.classList.toggle("selected");
+            choosen.classList.toggle("selected")
             prato = choosen
         } else {
-            prato.classList.remove("selected");
-            choosen.classList.toggle("selected");
+            prato.classList.remove("selected")
+            choosen.classList.toggle("selected")
             prato = choosen
         }
     }
     if (choosen.classList.contains("bebida")) {
         if (bebida === choosen) {
-            choosen.classList.toggle("selected");
+            choosen.classList.toggle("selected")
         } else if (bebida === undefined) {
-            choosen.classList.toggle("selected");
+            choosen.classList.toggle("selected")
             bebida = choosen
         } else {
-            bebida.classList.remove("selected");
-            choosen.classList.toggle("selected");
-            bebida = choosen;
+            bebida.classList.remove("selected")
+            choosen.classList.toggle("selected")
+            bebida = choosen
         }
     }
     if (choosen.classList.contains("sobremesa")) {
         if (sobremesa === choosen) {
-            choosen.classList.toggle("selected");
+            choosen.classList.toggle("selected")
         } else if (sobremesa === undefined) {
-            choosen.classList.toggle("selected");
+            choosen.classList.toggle("selected")
             sobremesa = choosen
         } else {
-            sobremesa.classList.remove("selected");
-            choosen.classList.toggle("selected");
-            sobremesa = choosen;
+            sobremesa.classList.remove("selected")
+            choosen.classList.toggle("selected")
+            sobremesa = choosen
         }
     }
     if (prato !== undefined && bebida !== undefined && sobremesa !== undefined) {
@@ -48,5 +48,14 @@ function select(choosen) {
             b.classList.remove("enable")
             b.innerHTML = "Selecione os 3 itens para fechar o pedido"
         }
+    }
+}
+
+function pedir(cliked) {
+    if (cliked.classList.contains("enable")) {
+        total = (Number(prato.querySelector("h4").querySelector("span").innerHTML.replace(",", ".")) + Number(bebida.querySelector("h4").querySelector("span").innerHTML.replace(",", ".")) + Number(sobremesa.querySelector("h4").querySelector("span").innerHTML.replace(",", "."))).toFixed(2)
+        message = encodeURIComponent("Olá, gostaria de fazer o pedido:\n- Prato: "+prato.querySelector("h3").innerHTML+"\n- Bebida: "+bebida.querySelector("h3").innerHTML+"\n- Sobremesa: "+sobremesa.querySelector("h3").innerHTML+"\nTotal: R$ "+total)
+        // alert(message)
+        location.href = "https://wa.me/5545998348694?text=" + message
     }
 }
