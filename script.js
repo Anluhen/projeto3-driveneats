5cb27491545ef2a065ddc3dd200f99b38dd879e0
André_Henchenski <andrehenc@gmail.com>
2022-08-14 14:39:36 -0300

    Projeto terminado com todos os bônus. Adicionado popup de confirmação.

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let prato = undefined
 let bebida = undefined
 let sobremesa = undefined
+let message
 
 function select(choosen) {
     if (choosen.classList.contains("prato")) {
@@ -53,9 +54,26 @@ function select(choosen) {
 
 function pedir(cliked) {
     if (cliked.classList.contains("enable")) {
-        total = (Number(prato.querySelector("h4").querySelector("span").innerHTML.replace(",", ".")) + Number(bebida.querySelector("h4").querySelector("span").innerHTML.replace(",", ".")) + Number(sobremesa.querySelector("h4").querySelector("span").innerHTML.replace(",", "."))).toFixed(2)
-        message = encodeURIComponent("Olá, gostaria de fazer o pedido:\n- Prato: "+prato.querySelector("h3").innerHTML+"\n- Bebida: "+bebida.querySelector("h3").innerHTML+"\n- Sobremesa: "+sobremesa.querySelector("h3").innerHTML+"\nTotal: R$ "+total)
-        // alert(message)
-        location.href = "https://wa.me/5545998348694?text=" + message
+        const total = (Number(prato.querySelector("h4").querySelector("span").innerHTML.replace(",", ".")) + Number(bebida.querySelector("h4").querySelector("span").innerHTML.replace(",", ".")) + Number(sobremesa.querySelector("h4").querySelector("span").innerHTML.replace(",", "."))).toFixed(2)
+        const nome = prompt("Qual é o seu nome?")
+        const endereco = prompt("Qual é o endereço de entrega?")
+        message = "Olá, gostaria de fazer o pedido:\n- Prato: " + prato.querySelector("h3").innerHTML + "\n- Bebida: " + bebida.querySelector("h3").innerHTML + "\n- Sobremesa: " + sobremesa.querySelector("h3").innerHTML + "\nTotal: R$ " + total + "\n\nNome: " + nome + "\nEndereço: " + endereco
+        const confirm = document.querySelector(".confirm")
+        confirm.classList.add("show")
+        confirm.querySelector(".c-prato").querySelector(".nome").innerHTML = prato.querySelector("h3").innerHTML
+        confirm.querySelector(".c-prato").querySelector(".preco").innerHTML = "R$ " + prato.querySelector("span").innerHTML
+        confirm.querySelector(".c-bebida").querySelector(".nome").innerHTML = bebida.querySelector("h3").innerHTML
+        confirm.querySelector(".c-bebida").querySelector(".preco").innerHTML = "R$ " + bebida.querySelector("span").innerHTML
+        confirm.querySelector(".c-sobremesa").querySelector(".nome").innerHTML = sobremesa.querySelector("h3").innerHTML
+        confirm.querySelector(".c-sobremesa").querySelector(".preco").innerHTML = "R$ " + sobremesa.querySelector("span").innerHTML
+        confirm.querySelector(".popup").querySelector(".total").querySelector(".valor").innerHTML = ("R$ " + total).replace(".",",")
     }
+}
+
+function send() {
+    location.href = "https://wa.me/5545998348694?text=" + encodeURIComponent(message)
+}
+
+function hide() {
+    document.querySelector(".confirm").classList.remove("show")
 }
